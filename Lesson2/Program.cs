ce5d0cb07c6061b9da88325cfe6970fa241c6104
HOME-PC\atanooniel <atepkina@gmail.com>
2024-02-04 17:17:53 +0300

    Домашнее задание 2

---
+++ Lesson2/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Lesson2;
+
+public class Program
+{
+    public static void Main()
+    {
+        
+        var input_number = Console.ReadLine();
+
+        var number = int.Parse(input_number);
+
+        if (number % 400 == 0)
+        { 
+            Console.WriteLine("YES"); 
+        }
+        else if (number % 4 == 0 && number % 100 != 0)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+
+    }
+}
