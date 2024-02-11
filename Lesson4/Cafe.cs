2bb1cdac83eea7c04f036e2e810a00f9134f2cfa
HOME-PC\atanooniel <atepkina@gmail.com>
2024-02-11 17:09:07 +0300

    Домашнее задание №4

---
+++ Lesson4/Cafe.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4;
+public class Cafe
+{
+    public string name;
+    public int rating;
+    public string? cousine;
+    public List<Visitor> visitors = new List<Visitor>();
+
+
+    public Cafe(string cafeName)
+    {
+        name = cafeName;
+    }
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Кафе с названием {name}, рейтинг: {rating}, кухня: {cousine}");
+    }
+
+    public void AddVisitor( Visitor visitor )
+    { 
+        visitors.Add( visitor );
+        Console.WriteLine($"В кафе \"{name}\" пришел посетитель {visitor.name}");
+    }
+
+    public void MakeOrder( Visitor visitor ) 
+    { 
+        Console.WriteLine($"Посетитель {visitor.name} заказал блюдо \"{visitor.favouriteMeal}\"");
+    }
+}
---
+++ Lesson4/Visitor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson4;
+public class Visitor
+{
+    public string name;
+    public int age;
+    public Cafe cafe;
+    public string favouriteMeal;
+
+    public Visitor(string visitorName, int visitorAge, Cafe visitorCafe, string visitorMeal)
+    {
+        name = visitorName;
+        age = visitorAge;
+        cafe = visitorCafe;
+        favouriteMeal = visitorMeal;
+    }
+
+
+    public void ShowInfo()
+    {
+        Console.WriteLine($"Посетитель с именем {name}, возраст: {age}");
+    }
+}
