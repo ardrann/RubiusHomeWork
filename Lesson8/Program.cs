e7e96d64079f174cd235160349af9a4b8aa7f607
HOME-PC\atanooniel <atepkina@gmail.com>
2024-03-03 16:23:15 +0300

    Домашнее задание 8, задание 1

---
+++ Lesson8/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Lesson8.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Counter counter = new Counter();
+            Handler1 handler1 = new Handler1();
+            Handler2 handler2 = new Handler2();
+
+            counter.Counting += handler1.Message;
+            counter.Counting += handler2.Message;
+
+            counter.Count();
+        }
+    }
+}
